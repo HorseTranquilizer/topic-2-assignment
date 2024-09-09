fb64009a7df5f70bc9613a26fe4cfdbaf7671fcc
339749939 <339749939@2260I-L29-015>
2024-09-09 09:23:16 -0400

    Add project files.

---
+++ topic 2 assignment/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace topic_2_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("John Doe");
+            Console.WriteLine("I was born on " + (2024 - 16));
+            Console.WriteLine("the average of 14 15 16 is " + (14 + 15 + 16 / 3));
+            Console.WriteLine("A circle with a radius of 3 has a area of " + (3 * 3.14 * 2 * 2));
+        }
+    }
+}
