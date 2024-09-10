756b5bf2dd413353ba9d5f2f8211da1f09e836de
Horse Tranquilizer <animeberd9@gmail.com>
2024-09-10 08:36:13 -0400

    Update Program.cs

--- topic 2 assignment/Program.cs
+++ topic 2 assignment/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace topic_2_assignment
+namespace topic_2_assignment
 {
     internal class Program
     {
@@ -8,6 +8,7 @@
             Console.WriteLine("I was born on " + (2024 - 16));
             Console.WriteLine("the average of 14 15 16 is " + (14 + 15 + 16 / 3));
             Console.WriteLine("A circle with a radius of 3 has a area of " + (3 * 3.14 * 2 * 2));
+            Console.WriteLine("It knows the difference by checking what is before the +, it checks if its a number or the end of a sentence");
         }
     }
 }
